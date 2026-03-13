398d5ffdb53f2886fd5d080dfdfc430f64e60974
LENOVO <LENOVO@DESKTOP-BB73UPO>
2026-03-13 17:39:08 +0530

    Commit 1(LoginPage)

---
+++ GroceryApplicationProject/src/test/java/testscripts/LoginTest.java
@@ -0,0 +1,58 @@
+package testscripts;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import automationcore.TestNGBase;
+import pages.LoginPage;
+import utilities.ExcelUtility;
+
+public class LoginTest extends TestNGBase
+{
+	
+	@Test
+	public void verifyUserLoginWithValidCredentials() throws IOException
+	{
+		 String userName=ExcelUtility.readStringData(0, 0, "LoginPage");
+		 String password=ExcelUtility.readStringData(0, 1, "LoginPage");
+		LoginPage login= new LoginPage(driver);
+		login.enterUserNameOnUserNameField(userName);
+		login.enterPasswordOnPasswordField(password);
+		login.clickLoginButton();
+	}
+	
+	@Test
+	public void verifyUserLoginWithInvalidUserName() throws IOException
+	{
+		 String userName=ExcelUtility.readStringData(1, 0, "LoginPage");
+		 String password=ExcelUtility.readStringData(1, 1, "LoginPage");
+		LoginPage login= new LoginPage(driver);
+		login.enterUserNameOnUserNameField(userName);
+		login.enterPasswordOnPasswordField(password);
+		login.clickLoginButton();
+	}
+	
+	@Test
+	public void  verifyUserLoginWithInvalidPassword() throws IOException
+	{
+		 String userName=ExcelUtility.readStringData(2, 0, "LoginPage");
+		 String password=ExcelUtility.readStringData(2, 1, "LoginPage");
+		LoginPage login= new LoginPage(driver);
+		login.enterUserNameOnUserNameField(userName);
+		login.enterPasswordOnPasswordField(password);
+		login.clickLoginButton();
+	}
+	
+	@Test
+	public void verifyUserLoginWithInvalidCredentials() throws IOException
+	{
+		 String userName=ExcelUtility.readStringData(3, 0, "LoginPage");
+		 String password=ExcelUtility.readStringData(3, 1, "LoginPage");
+		LoginPage login= new LoginPage(driver);
+		login.enterUserNameOnUserNameField(userName);
+		login.enterPasswordOnPasswordField(password);
+		login.clickLoginButton();
+	}	
+
+}
